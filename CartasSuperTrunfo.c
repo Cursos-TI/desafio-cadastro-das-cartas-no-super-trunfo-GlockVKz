9eeda52328aa89372dc04bcd98934b4790e06764
GlockVKz <victor2andre17@gmail.com>
2025-01-08 20:53:54 +0000

    add nome da cidade

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    printf("qual o codigo da cidade?");
-    scanf("689");
-    prinft("quantas pessoas tem nessa cidade?");
-    scanf("64 mil pessoas");
+    int codigo;
+    int populacao;
+    int nomecidade;
+
+    printf("Qual o codigo da cidade? ");
+    scanf("%d", &codigo); // Lê um número inteiro para o código da cidade
+
+    printf("Quantas pessoas tem nessa cidade? ");
+    scanf("%d", &populacao); // Lê o número de pessoas na cidade
+
+    printf("Qual o nome da cidade? ");
+    scanf("%s", &nomecidade);
+
+    printf("Cidade com o nome %s tem o código %d tem %d pessoas .\n", nomecidade, codigo, populacao);
+
     }
+
