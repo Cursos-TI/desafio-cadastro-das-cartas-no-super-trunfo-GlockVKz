609c42f131da1cb691a3927605c01d5b492467df
GlockVKz <victor2andre17@gmail.com>
2025-01-20 17:00:54 +0000

    retirei space

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,13 +5,13 @@ int main() {
     int populacao;
     char nomecidade[50];
 
-    printf("Qual o codigo da cidade?\n ");
+    printf("Qual o codigo da cidade?\n");
     scanf("%d", &codigo); // Lê um número inteiro para o código da cidade
 
-    printf("Quantas pessoas tem nessa cidade?\n ");
+    printf("Quantas pessoas tem nessa cidade?\n");
     scanf("%d", &populacao); // Lê o número de pessoas na cidade
 
-    printf("Qual o nome da cidade?\n ");
+    printf("Qual o nome da cidade?\n");
     scanf("%s", &nomecidade);
 
     printf("Cidade com o nome %s tem o código %d tem %d pessoas .\n", nomecidade, codigo, populacao);
