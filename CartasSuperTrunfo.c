47127f47185632a40155b74c60969490cb2bca7f
GlockVKz <victor2andre17@gmail.com>
2025-02-20 20:05:09 +0000

    add mais cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 
 int main() {
-    int codigo;
-    int populacao;
-    char nomecidade[50];
+    char estado[10]; //ok
+    int codigo; //ok
+    int numerodepontos; //ok
+    int populacao; //ok
+    int area; //ok
+    int pib; //okk
+    char nomecidade[50]; //ok
+
+    printf("Qual o nome do estado?\n");
+    scanf("%s", &estado); //nome do estado
 
     printf("Qual o codigo da cidade?\n");
-    scanf("%d", &codigo); // Lê um número inteiro para o código da cidade
+    scanf("%d", &codigo); // numero do codigo
+
+    printf("Qual o nome da cidade?\n");
+    scanf("%s", &nomecidade); //nome da cidade
 
     printf("Quantas pessoas tem nessa cidade?\n");
     scanf("%d", &populacao); // Lê o número de pessoas na cidade
 
-    printf("Qual o nome da cidade?\n");
-    scanf("%s", &nomecidade);
+    printf("Qual a area da cidade?\n");
+    scanf("%d", &area); // area da cidade
 
-    printf("Cidade com o nome %s tem o código %d tem %d pessoas .\n", nomecidade, codigo, populacao);
+    printf("Qual o PIB da cidade?\n");
+    scanf("%d", &pib); // PIB DA CIDADE
+
+    printf("Qual Número de Pontos Turísticos da cidade?\n");
+    scanf("%d", &numerodepontos); // pontos turisticos da cidade
+
+    printf("carta1:\n Estado:%s\n",estado);
 
     }
 
